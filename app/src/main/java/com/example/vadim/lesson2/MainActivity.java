973d28dede42d8cc96534d579638e88114c05e03
Vadims Zilnieks <vadim@Irinas-MacBook.local>
2018-06-03 16:31:42 +0300

    two activities

--- app/src/main/java/com/example/vadim/lesson2/MainActivity.java
+++ app/src/main/java/com/example/vadim/lesson2/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+//    setContentView(R.layout.activity_main_login);
   }
 
   public void onLoginClick(View view) {
